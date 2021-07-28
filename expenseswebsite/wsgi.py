e09090495f417af5d7231ea25d985e4d3a8e69ff
Geetika <67047991+Geetika-Raut@users.noreply.github.com>
2021-07-28 22:01:29 +0530

    Update wsgi.py

--- expenseswebsite/wsgi.py
+++ expenseswebsite/wsgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+from django.core.wsgi import get_wsgi_application
+import sys
+
+# assuming your Django settings file is at
+# '/home/myusername/mysite/mysite/settings.py'
+path = '/home/geetikaraut/ExpenseSite'
+if path not in sys.path:
+     sys.path.insert(0, path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'expenseswebsite.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'expenseswebsite.settings')
-
 application = get_wsgi_application()
+
+
